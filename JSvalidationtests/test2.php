301c20005eb48ec8b5969b1d63dd9dad7e7f389b
Nicole <ns21549n@pace.edu>
2014-02-21 19:09:00 -0500

    JSvalidationstests: test and test2 used to ensure the user is not trying to submit the text box without uploading code. Next test ensures user uploads a file and they upload a php file. Thirdtest(unfinished) attempts to check the size of the file but doesn't currently work and needs to be corrected

---
+++ JSvalidationtests/test2.php
@@ -0,0 +1,23 @@
+
+<html>
+
+<?php
+//create short variable names
+$firstname = ($_POST['fname']);
+?>
+
+
+<head>
+	<title>Your Name is:</title>
+</head>
+<body>
+	<?php
+
+	echo "<p>Your Names are: </p>";
+	echo $firstname. " is your first name</br>";
+	?>
+
+</body>
+
+
+</html>
