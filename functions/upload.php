0362fa8f27915ef8258320200b7edecc43912d27
Shane <droidburgundy@gmail.com>
2014-02-24 09:36:04 -0500

    added upload back

--- functions/upload.php
+++ functions/upload.php
@@ -4,6 +4,7 @@
 $upload_dir = "../UploadedFiles/";
 
 if (!is_dir($upload_dir)) { mkdir($upload_dir, 0755);}
+// - if creating the dir fails and dir is not there - we need to handle that error
 
 //array to set allowed file types
 
@@ -50,7 +51,7 @@ if (!empty($_FILES["input"])) {
     	post_max_size = 8M
 		upload_max_size = 2M
 		max_file_uploads = 20
-		ALSO - look into adding virus scanning - google ClamAV + PHP ? WHat ar ethe other alternatives?*/
+		ALSO - look into adding virus scanning - google ClamAV + PHP ? What ar ethe other alternatives?*/
 	} else { 
 		echo "<p>Unable to save file. The file must be of following type(s): ";
 		foreach ($allowedexts as $allowedext) { print_r($allowedext); } 
