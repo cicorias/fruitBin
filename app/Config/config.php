b433c2ae310992d8230278f96f022ab3c8415f6e
Shane <droidburgundy@gmail.com>
2014-03-28 16:58:08 -0400

    removed uneeded classes

--- app/Config/config.php
+++ app/Config/config.php
@@ -1,6 +1,21 @@
 <?php
+/**
+ * Configuration for: Error reporting
+ * Useful to show every little problem during development, but only show hard errors in production
+ */
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+/**
+ * Configuration for: Project URL
+ * Put your URL here, for local development "127.0.0.1" or "localhost" (plus sub-folder) is fine
+ */
+define('URL', 'http://127.0.0.1/php-mvc/');
+
 //start a session for use in getting inputs
-session_start();
+if (!isset($_SESSION)) {
+    session_start();
+}
 
 $GLOBALS['config'] = array(
 	'mysql' => array(
