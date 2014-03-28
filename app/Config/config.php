cb2c22c906006d136cc6c08dc52a7e5925438687
Shane <droidburgundy@gmail.com>
2014-03-28 17:07:41 -0400

    changed mvc layout

--- app/Config/config.php
+++ app/Config/config.php
@@ -1,39 +1,35 @@
 <?php
 /**
- * Configuration for: Error reporting
+ * Error reporting
  * Useful to show every little problem during development, but only show hard errors in production
  */
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 /**
- * Configuration for: Project URL
- * Put your URL here, for local development "127.0.0.1" or "localhost" (plus sub-folder) is fine
+ * Project URL
+ * local development == "127.0.0.1" or "localhost" (plus sub-folder)
+ * hosted == "fruitify.io"
  */
-define('URL', 'http://127.0.0.1/php-mvc/');
+define('URL', 'http://127.0.0.1/fruitBin/');
 
 //start a session for use in getting inputs
 if (!isset($_SESSION)) {
     session_start();
 }
 
-$GLOBALS['config'] = array(
-	'mysql' => array(
-		'host' => 'localhost',
-		'username' => '',
-		'password' => '',
-		'db' => ''),
-	'folders' => array(
-		'uploads' => '../UploadedFiles/')
-	/*'session' => array(
-		'session_name' => 'user',
-		'token_name' => 'token')*/);
+define('DB_TYPE', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+
+//DIRS & PATHS
+define('TMP_DIR', '../../TmpFiles/');
+$upload_dir = TMP_DIR;
 
 //autoload every OOP Class in directory 
 spl_autoload_register(function($class) {require_once '../Classes/'.$class.'.class.php';} );
-
-//set global Upload dir
-$upload_dir = Config::get('folders/uploads');
 
 
 ?>
