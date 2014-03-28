9413264d9c0b27855be7b0a5c1931e116379d77a
Shane <droidburgundy@gmail.com>
2014-03-28 17:37:32 -0400

    changed again

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
 /**
- * A simple PHP MVC skeleton
+ * Fruitify Main Index
  *
- * @package php-mvc
- * @author Panique
- * @link http://www.php-mvc.net
- * @link https://github.com/panique/php-mvc/
+ * @package fruitBin
+ * @author Fruitify
+ * @link https://github.com/Fruitify
+ * @link https://github.com/Fruitify/fruitBin
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
-// load the (optional) Composer auto-loader
+// load the (optional) Composer auto-loader (if we end up needing composer)
 if (file_exists('vendor/autoload.php')) {
     require 'vendor/autoload.php';
 }
 
-// load application config (error reporting etc.)
-require 'application/config/config.php';
+// load fruitify configuration 
+require 'app/config/config.php';
 
 // load application class
-require 'application/libs/application.php';
-require 'application/libs/controller.php';
+require 'app/libs/application.php';
+require 'app/libs/controller.php';
 
 // start the application
 $app = new Application();
